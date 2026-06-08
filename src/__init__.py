a97ffe883e760838e445f1d862ec3a0e2040bf11
estelar-latam <maicoldouglasvelezvivanco@gmail.com>
2026-06-08 09:06:05 -0500

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+X-Tweet-Analyzer
+================
+A powerful X/Twitter profile analyzer and tweet scraper.
+Extracts, classifies, and exports all tweets from any public profile.
+
+Author: estelar-latam
+License: MIT
+Version: 1.0.0
+"""
+
+__version__ = "1.0.0"
+__author__ = "estelar-latam"
+__license__ = "MIT"
